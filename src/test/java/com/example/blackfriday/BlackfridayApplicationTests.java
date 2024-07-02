02882357b11a20dcc3c292a27cc81e64a605b66a
syh24 <dbsgur6896@naver.com>
2024-07-02 11:32:30 +0900

    feat: initial commit

---
+++ src/test/java/com/example/blackfriday/BlackfridayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blackfriday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackfridayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
