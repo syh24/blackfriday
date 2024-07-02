04d159a51711c234f220563997c1b0e2d157eceb
syh24 <dbsgur6896@naver.com>
2024-07-02 13:57:16 +0900

    test: MemberControllerTest, MemberServiceTest 작성

--- src/main/java/com/example/blackfriday/controller/dto/MemberDto.java
+++ src/main/java/com/example/blackfriday/controller/dto/MemberDto.java
@@ -23,7 +23,7 @@ public class MemberDto {
         @NotNull(message = "이름을 입력해주세요.")
         private String name;
         @NotNull(message = "전화번호를 입력해주세요.")
-        @Pattern(message = "전화번호 포멧을 확인해주세요 '010-123-1234' 또는 '010-1234-1234'", regexp = "^\\d{3}-\\{d{3,4}-\\d{4}$")
+        @Pattern(message = "전화번호 포멧을 확인해주세요 '010-123-1234' 또는 '010-1234-1234'", regexp = "^\\d{3}-\\d{3,4}-\\d{4}$")
         private String phone;
         @NotNull(message = "회원권한을 입력해주세요.")
         private Role role;
