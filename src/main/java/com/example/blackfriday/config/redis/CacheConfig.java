d9f6f7c21b1d9880a53784059122d72f240d9d59
syh24 <dbsgur6896@naver.com>
2024-07-15 12:10:27 +0900

    refactor: 레디스 캐시 서버와 세션 서버 분리

--- src/main/java/com/example/blackfriday/config/redis/CacheConfig.java
+++ src/main/java/com/example/blackfriday/config/redis/CacheConfig.java
@@ -1,12 +1,16 @@
 package com.example.blackfriday.config.redis;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -18,8 +22,6 @@ import java.time.Duration;
 @Configuration
 public class CacheConfig {
 
-    private final RedisConnectionFactory redisConnectionFactory;
-
     /**
      * Redis Cache 설정
      * 캐시 TTL 30분
@@ -27,7 +29,7 @@ public class CacheConfig {
      * @return
      */
     @Bean
-    public RedisCacheManager redisCacheManager() {
+    public RedisCacheManager redisCacheManager(@Qualifier("redisCacheConnectionFactory") RedisConnectionFactory redisConnectionFactory) {
         RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
                 .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
